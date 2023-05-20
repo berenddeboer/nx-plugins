555e0d8fe27500bf045b8522e27d06e41bcde124
Berend de Boer <berend@pobox.com>
2023-05-20 16:01:05 +1200

    refactor: cleanup code a little

--- e2e/nx-aws-cdk-e2e/jest.config.js
+++ e2e/nx-aws-cdk-e2e/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  displayName: 'nx-aws-cdk-e2e',
-  preset: '../../jest.preset.js',
+  displayName: "nx-aws-cdk-e2e",
+  preset: "../../jest.preset.js",
   globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
     },
   },
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    "^.+\\.[tj]s$": "ts-jest",
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/e2e/nx-aws-cdk-e2e',
-};
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/e2e/nx-aws-cdk-e2e",
+}
--- packages/nx-aws-cdk/jest.config.ts
+++ packages/nx-aws-cdk/jest.config.ts
@@ -1,10 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'nx-aws-cdk',
-  preset: '../../jest.preset.js',
+  displayName: "nx-aws-cdk",
+  preset: "../../jest.preset.js",
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/nx-aws-cdk',
-};
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+}
